8954920a344ecaad08507fb2ea27d3f0c6372a18
yomali <yomalisenevirathne2003@gmail.com>
2026-04-03 23:37:57 +0530

    commnt3

--- backend/src/main/java/smart_op_hub/CampusHub/controller/TestController.java
+++ backend/src/main/java/smart_op_hub/CampusHub/controller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import smart_op_hub.CampusHub.model.User;
 import smart_op_hub.CampusHub.repository.UserRepository;
 
-@CrossOrigin(origins = "http://localhost:5174") // aluth. React. Port ekata permission deema
+@CrossOrigin(origins = "http://localhost:5174") // aluth. React. Port. ekata permission deema
 @RestController
 @RequestMapping("/test")
 public class TestController {
